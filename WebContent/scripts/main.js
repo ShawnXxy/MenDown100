9b04159724dd52d70771c2011c832176225d84c9
ShawnXxy <shawnxxy@hotmail.com>
2017-07-28 17:36:33 +0800

    HTML

---
+++ WebContent/scripts/main.js
@@ -0,0 +1,27 @@
+/***********
+ *  Initializing
+ ************/
+var main = {
+		gameInfo : { // game windows size
+			width : 0,
+			height : 0
+		},
+		context : null,
+		person : null,
+		timeQueue : null, 
+		time : 0,
+		leveltime : 0,
+		level : 0,
+		imgs : [],
+		blocks : [],
+		init : function() {
+			main.init();
+		}
+}
+main.init();
+
+
+
+/*********************************
+**	Blocks/Bricks/Barriers in the game
+**********************************/
