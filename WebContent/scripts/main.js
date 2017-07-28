f1ad7771ac5e9dfadf1d3ffc26fa8a2602076574
ShawnXxy <shawnxxy@hotmail.com>
2017-07-28 22:01:28 +0800

    to be continued

--- WebContent/scripts/main.js
+++ WebContent/scripts/main.js
@@ -15,11 +15,42 @@ var main = {
 		imgs : [],
 		blocks : [],
 		init : function() {
-			main.init();
+			main.initStart();
+		},
+		initStart : function() {
+			main.initData();
+		},
+		initData : function() {
+			WF.file.imgs(["img/man.png", "img/block.png", "img/move.png", "img/thorn.png", "img/flip.png"], function(img) {
+				main.imgs = imgs;
+				var canvas = WF.getId("canvas");
+				main.gameInfo.width = canvas.offsetWidth;
+				main.gameInfo.height = canvas.offsetHeight;
+				main.context = canvas.getContext("2d");
+				WF.getId("js_start_loading").style.display = "none";
+				WF.getId("js_start_btn").style.display = "block";
+			});
 		}
 }
 main.init();
 
+var WF = function() {
+	function getId(id) {
+		return typeof id == "string" ? document.getElementById(id) : id;
+	};
+	function reg(space, obj) {
+		var nameSpace = exports[space] || {};
+		for (var key in obj) {
+			nameSpace[key] = obj[key];
+		}
+		exports[space] = nameSpace;
+	}
+	var exports = {
+			getId : getId,
+			reg : reg
+	};
+	return exports;
+}();
 
 
 /*********************************
